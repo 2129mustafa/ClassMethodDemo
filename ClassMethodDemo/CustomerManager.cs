63f4991a7ddba1c2c09a7a079086b1b93250ae54
MUSTAFA YENAL <202803018@ogr.cbu.edu.tr>
2021-04-01 12:30:05 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName+" added.");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " updated.");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " deleted.");
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "Mustafa";
+            customer1.LastName = "Yenal";
+            customer1.City = "Balıkesir";
+
+            Customer customer2 = new Customer()//farklı bir kullanım
+            {
+                Id = 2,
+                FirstName = "Engin",
+                LastName = "Demiroğ",
+                City = "Ankara"
+            };
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.LastName = "Burak";
+            customer3.FirstName = "Kara";
+            customer3.City = "İstanbul";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            Console.WriteLine("---------------------------\n");
+            customerManager.Update(customer2);
+            Console.WriteLine("---------------------------\n");
+            customerManager.Delete(customer3);
+            Console.WriteLine("---------------------------\n");
+
+
+        }
+    }
+}
